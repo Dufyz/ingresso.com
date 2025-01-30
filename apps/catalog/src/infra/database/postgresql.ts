0e848460feb2c9bd0fd1df2fcdd30b62877b0000
dufyz <guilhermeiagothomaz@hotmail.com>
2025-01-29 21:21:38 -0300

    feat: Add error handling, configuration, and database setup for Catalog Service

---
+++ apps/catalog/src/infra/database/postgresql.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import postgres from 'postgres';
+import { DB_CONNECTION, DB_TEST_CONNECTION, NODE_ENV } from '../config';
+
+if (!DB_CONNECTION) throw new Error('Check DB_CONNECTION env');
+
+const sql = postgres(NODE_ENV === 'test' ? DB_TEST_CONNECTION : DB_CONNECTION, {
+  prepare: false,
+  // debug(connection, query, parameters, paramTypes) {
+  //   console.log({ connection, query, parameters });
+  // },
+  connect_timeout: 6000,
+  idle_timeout: 8000,
+  onclose(connId) {
+    console.log(`Connection ${connId} closed`);
+  },
+});
+
+const query = sql``;
+
+export type SQLQuery = typeof query;
+
+export default sql;
--- apps/catalog/src/main.ts
+++ apps/catalog/src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { SERVER_PORT } from './infra/config';
+
+import {
+  ValidationPipe,
+  ClassSerializerInterceptor,
+  Logger,
+} from '@nestjs/common';
+import { HttpLoggerInterceptor } from './presentation/httpLogger.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger('main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new HttpLoggerInterceptor());
+
+  await app.listen(SERVER_PORT);
+
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+void bootstrap();
